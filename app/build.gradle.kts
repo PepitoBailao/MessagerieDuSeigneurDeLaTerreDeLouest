508faba36af0b5106900dd4db990426ac546991d
Ulysse <151675412+PepitoBailao@users.noreply.github.com>
2024-10-16 15:16:17 +0200

    Nouvelles images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,18 @@ android {
 }
 
 dependencies {
+    // Firebase Authentication
+    implementation("com.google.firebase:firebase-auth:21.0.1")
+    // Bibliothèque pour ajuster la taille des éléments en fonction de la densité d'écran
+    implementation("com.intuit.sdp:sdp-android:1.1.1")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.firebase.database)
-    implementation(libs.firebase.auth)
-    implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
